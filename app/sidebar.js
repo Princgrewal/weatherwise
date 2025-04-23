40d577e76d14b2a95ffee6ae971c3349e3fb0dc7
Princgrewal <164895925+Princgrewal@users.noreply.github.com>
2025-04-22 21:59:26 -0600

    used link

--- app/sidebar.js
+++ app/sidebar.js
@@ -1,24 +1,26 @@
+import Link from "next/link";
+
 export default function Sidebar() {
   return (
-    <div className="w-64 h-screen bg-gradient-to-b from-blue-700 to-indigo-900 text-white p-6 shadow-lg backdrop-blur-md ">
+    <div className="w-64 h-screen bg-gradient-to-b from-blue-700 to-indigo-900 text-white p-6 shadow-lg backdrop-blur-md">
       <h2 className="text-3xl font-bold mb-8 tracking-wide">WeatherWise</h2>
       <nav>
         <ul className="space-y-4">
           <li>
-            <a
+            <Link
               href="/"
               className="block px-4 py-2 rounded-lg hover:bg-blue-500 transition duration-200 hover:scale-105"
             >
               Home
-            </a>
+            </Link>
           </li>
           <li>
-            <a
+            <Link
               href="/settings"
               className="block px-4 py-2 rounded-lg hover:bg-blue-500 transition duration-200 hover:scale-105"
             >
               Settings
-            </a>
+            </Link>
           </li>
         </ul>
       </nav>
